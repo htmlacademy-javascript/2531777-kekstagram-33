5588ebcf18873a5d5c10cbacb945c63e37d853e4
Смаглюк Екатерина <kijtos@mail.ru>
2024-10-30 23:31:32 +0300

    Выполняет домашнее задание №2

--- js/functions.js
+++ js/functions.js
@@ -17,7 +17,7 @@ const isPalindrome = (string) => {
   string = string.replaceAll(' ','').toLowerCase();
   let emptyString = '';
 
-  for (let i = string.length-1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     emptyString += string(i);
   }
   return string === emptyString;
